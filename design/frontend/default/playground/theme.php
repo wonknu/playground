e3a87b99c6541222740f5d213859e6cb10dcee5a
GrG <gregory.besson@adfab.fr>
2013-09-29 17:55:02 +0200

    Playground Theme is on its way !

---
+++ design/frontend/default/playground/theme.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'design' => array(
+        'package' => array(
+            'code' => 'default',
+            'title' => 'default',
+            'theme' => array(
+                'code' => 'playground',
+                'title' => 'Playground',
+                'parent' => 'playground_base',
+            )
+        ),
+    ),
+);
