13c244eed8de3ff9a05761e1dc58812f9ae647be
GrG <gregory.besson@adfab.fr>
2013-10-07 09:56:56 +0200

    opengraph

---
+++ config/autoload/playgroundflow.global.php
@@ -0,0 +1,20 @@
+<?php
+$playgroundflow = array(
+		
+		/**
+		 * Drive path to the directory where story media files will be stored
+		 */
+		'media_path' => 'public' . DIRECTORY_SEPARATOR . 'media' . DIRECTORY_SEPARATOR . 'story',
+		
+		/**
+		 * Url relative path to the directory where story media files will be stored
+		 */
+		'media_url' => 'media/story',
+);
+
+/**
+ * You do not need to edit below this line
+ */
+return array(
+    'playgroundflow' => $playgroundflow,
+);
