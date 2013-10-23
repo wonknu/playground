35ce5f4c873043c81f6b577df65a1b513aeb0b00
Thomas ROGER <thomas.roger@adfab.fr>
2013-10-23 18:22:17 +0200

    add install demo for unix, playground-304

---
+++ install_demo.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+ENV="prod"
+if [ $1 ]
+then
+    database="$1"
+fi
+ 
+echo "get data for playground demo"
+git clone https://github.com/gregorybesson/playground-demo.git /var/tmp/playground-demo > /dev/null
+cp -R /var/tmp/playground-demo/media/* public/media/.
+chmod -R 777 public/media
+
+php vendor/doctrine/doctrine-module/bin/doctrine-module.php orm:schema-tool:drop --force
+php vendor/doctrine/doctrine-module/bin/doctrine-module.php dba:import /var/tmp/playground-demo/sql/demo.sql
+php vendor/doctrine/doctrine-module/bin/doctrine-module.php orm:schema-tool:update --force
+php public/index.php assetic setup
+php public/index.php assetic build
+
+rm -rf /var/tmp/playground-demo
