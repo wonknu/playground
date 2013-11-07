27ba81ac45e05b1330e0aea162789d7bc64b03a2
Thomas ROGER <thomaroger@gmail.com>
2013-11-07 22:15:55 +0100

    add active / inactive mission + dont show game in mission

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -74,9 +74,9 @@ class IndexController extends AbstractActionController
 
         $layoutViewModel->addChild($slider, 'slider');
 
-        $games = $this->getGameService()->getActiveGames();
+        $games = $this->getGameService()->getActiveGamesWithoutGamesInMission();
         $pages = $this->getPageService()->getActivePages();
-        $missions = $this->getMissionService()->getMissions();
+        $missions = $this->getMissionService()->getActiveMissions();
 
         // I merge both types of articles and sort them in reverse order of their key
         // And as their key is some sort of... date !, It means I sort it in date reverse order ;)
